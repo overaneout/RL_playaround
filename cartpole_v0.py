57330e04d3e20c44dea1786a4f0b1dd0f37a41f3
Lee Ying Yang <overaneout@gmail.com>
2019-07-27 17:20:27 +0800

    Added plotting for score and 100 average

--- cartpole_v0.py
+++ cartpole_v0.py
@@ -1,22 +1,22 @@
 import gym
 import numpy as np
+import matplotlib.pyplot as plt
 
 # First approach
 # use simple q-learning
 LEARNING_RATE = 0.5
 DISCOUNT = 0.999
-EPISODES = 700
+EPISODES = 1000
 dimensions = [5, 5, 12, 5]
 EPSILON = 0.8
-EPSILON_DECAY_STOP_EP = int(4/5 * EPISODES)
+EPSILON_DECAY_STOP_EP = 700
 EPSILON_DECAY = EPSILON/EPSILON_DECAY_STOP_EP
 
 # set q-table
-# env state has formate of
+# env state has format of
 # [cart_position, cart_velocity, pole_angle, pole_tip_velocity]
 upper_bounds = np.array([4.8000002e+00, 5, 4.1887903e-01, 5])
 lower_bounds = np.array([-4.8000002e+00, -5, -4.1887903e-01, -5])
-
 q_table = np.random.uniform(low=0, high=1, size=dimensions + [2])
 discrete_windows_sizes = (upper_bounds - lower_bounds) / dimensions
 
@@ -26,14 +26,12 @@ def get_q_position(state, lower_bounds):
     return tuple(q_position.astype(np.int))
 
 
-# set hyper-parameters
-
 with gym.make("CartPole-v0") as env:
+    ep_survival = []
     for ep in range(EPISODES):
         state = env.reset()
         discrete_state = get_q_position(state, lower_bounds)
         done = False
-        reward = 0
         steps = 0
         while not done:
             steps += 1
@@ -53,7 +51,14 @@ with gym.make("CartPole-v0") as env:
             discrete_state = new_discrete_state
             if ep > (EPISODES*99)//100:
                 env.render()
+        EPSILON -= EPSILON_DECAY
         print(f"current ep {ep}")
         print(steps)
-        EPSILON -= EPSILON_DECAY
+        ep_survival.append(steps)
 
+
+moving_averages = [0]*99 +[np.mean(ep_survival[i-100:i]) for i in range(100,EPISODES+1)]
+history = {"episode": [i for i in range(EPISODES)],
+           "score": ep_survival,
+           "averages": moving_averages}
+plt.plot(history["episode"], history["score"], "r", history["episode"], history["averages"], "b")
